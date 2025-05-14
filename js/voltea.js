fd5db3cc31eaf963649bf470baa3bfcea71eb946
camilo.amezquita <jcaopita@gmail.com>
2025-05-14 12:24:37 -0500

    feat: :sparkles: diseño de targetas

--- js/voltea.js
+++ js/voltea.js
@@ -1,16 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
     const tarjetas = document.querySelectorAll('.tarjeta');
     const personajes = [
-        { nombre: "Son Goku", info: "El Saiyan más poderoso del universo 7.", transformaciones: 10 },
-        { nombre: "Vegeta", info: "Príncipe de los Saiyans y rival de por vida de Goku.", transformaciones: 8 },
-        { nombre: "Son Gohan", info: "Hijo de Goku, padre de pan, defensor de la Tierra.", transformaciones: 7 },
-        { nombre: "Trunks", info: "Hijo de Vegeta y Bulma, viajero del tiempo.", transformaciones: 3 },
-        { nombre: "Broly", info: "Broly es un Saiyajin de clase baja con un poder descomunal que supera los límites naturales de su raza.", transformaciones: 4 },
+        { nombre: "Naruto", info: "crece como huérfano y rechazado por tener al zorro de nueve colas sellado en su interior, lucha por ganarse el respeto de todos.", transformaciones: 6 },
+        { nombre: "Sasuke", info: "es un prodigioso ninja del clan Uchiha, conocido por su habilidad en el combate y su deseo de vengar la muerte de su clan a manos de su hermano, Itachi.", transformaciones: 6 },
+        { nombre: "Itachi", info: "es un ninja prodigioso y miembro del clan Uchiha, conocido por su inteligencia y habilidades en el combate.", transformaciones: 3 },
+        { nombre: "Kakashi", info: "es un ninja talentoso y líder del Equipo 7 en Naruto, conocido por su inteligencia, habilidades tácticas y su Sharingan (que obtuvo de un amigo caído, Obito Uchiha).", transformaciones: 4 },
+        { nombre: "Obito", info: "fue un ninja del clan Uchiha que soñaba con convertirse en Hokage, pero tras una tragedia que lo marcó profundamente, se convirtió en uno de los principales antagonistas de Naruto Shippuden.", transformaciones: 4 },
     ];
 
     tarjetas.forEach((tarjeta, index) => {
         const button = tarjeta.querySelector('button');
-        const imagenOriginal = tarjeta.querySelector('img')?.src; // <-- Guardar src antes
+        const imagenOriginal = tarjeta.querySelector('img')?.src;
 
         button.addEventListener('click', () => {
             tarjeta.classList.toggle('volteada');
@@ -27,7 +27,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
                 tarjeta.querySelector('.volver').addEventListener('click', () => {
                     tarjeta.classList.remove('volteada');
-
                     tarjeta.innerHTML = `
                         <div class="tarjeta-frontal">
                             <img src="${imagenOriginal}" alt="${personajes[index].nombre}" />
@@ -42,7 +41,3 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
-
-
-
-
